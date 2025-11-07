2e34d4f7f2ae7dff82241c6f078e23ff55800884
NicoMore02 <nicomore053@gmail.com>
2025-11-07 16:55:01 -0300

    cuarto commit

--- Repositorios/ProductosRepository.cs
+++ Repositorios/ProductosRepository.cs
@@ -88,6 +88,12 @@ public class ProductosRepository
         using var conexion = new SqliteConnection(connectionString);
         conexion.Open();
 
+        string sqlEliminarDetalles = "DELETE FROM PresupuestosDetalle WHERE idProducto = @id";
+        using var cmdDetalles = new SqliteCommand(sqlEliminarDetalles, conexion);
+        cmdDetalles.Parameters.Add(new SqliteParameter("@id", id));
+        cmdDetalles.ExecuteNonQuery();
+
+
         string sql = "DELETE FROM Productos WHERE idProducto = @id";
         using var comando = new SqliteCommand(sql, conexion);
         comando.Parameters.Add(new SqliteParameter("@id", id));
